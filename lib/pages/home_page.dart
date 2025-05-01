5481256a54290c8996fafd7e40dee549ba0f4aae
Matheus Higino <matheus.higino@yahoo.com.br>
2025-05-01 16:28:10 -0300

    apresentando o Column

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,26 +10,43 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var quantityClicks = 0;
   var randomNumber = 0;
 
   @override
   Widget build(BuildContext context) {
-    debugPrint("O método build é chamado cada vez que o estado deste objeto é alterado");
+    debugPrint(
+        "O método build é chamado cada vez que o estado deste objeto é alterado");
     return Scaffold(
       appBar: AppBar(
-        title: Text("Meu App", 
+        title: Text(
+          "Meu App",
           // style: GoogleFonts.abyssinicaSil(),
         ),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: Center(child: Text(
-          randomNumber.toString(),
-          style: TextStyle(fontSize: 20), 
-          // style: GoogleFonts.acme(fontSize: 20),
-        )),
+      body: Container(
+        margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text("Ações do usuário", style: TextStyle(fontSize: 20)),
+            Text("Foi clicado $quantityClicks vezes",
+                style: TextStyle(fontSize: 20)),
+            Text(
+              "O número gerado foi: $randomNumber",
+              style: TextStyle(fontSize: 20),
+              // style: GoogleFonts.acme(fontSize: 20),
+            )
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
           setState(() {
+            quantityClicks++;
             randomNumber = Utils.generateIntRandomNumber(1000);
           });
         },
