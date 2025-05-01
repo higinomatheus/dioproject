3808fedec26eb02a037807037df6d0afc86623c7
Matheus Higino <matheus.higino@yahoo.com.br>
2025-05-01 10:10:31 -0300

    gerando números aleatórios

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main(){
@@ -28,12 +30,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var randomNumber = 0;
+
+  void _generateRandomNumber(){
+    Random num = Random();
+    setState(() {
+      randomNumber = num.nextInt(1000);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    debugPrint("O método build é chamado cada vez que o estado deste objeto é alterado");
     return Scaffold(
       appBar: AppBar(
         title: const Text("Meu App"),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(child: Text(randomNumber.toString())),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _generateRandomNumber,
+        child: Icon(Icons.add_box),
       ),
     );
   }
